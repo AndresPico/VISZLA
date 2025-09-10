cf02917b74b18016947a71d4319adb57a9d7438b
AndresPico <2005picogarcia@gmail.com>
2025-09-09 22:14:15 -0500

    Fixes

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en Puerto: ${PORT}`);
 });
 
 app.use((req, res, next) => {
