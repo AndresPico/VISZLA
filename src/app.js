dc23d4f8c18369d7e9857a8d9ed0fddf5881ed43
AndresPico <2005picogarcia@gmail.com>
2025-09-30 21:02:20 -0500

    Arreglo de CORS

--- src/app.js
+++ src/app.js
@@ -10,6 +10,12 @@ connectDB();
 
 const app = express();
 
+// ✅ Configuración de CORS
+app.use(cors({
+  origin: "*", 
+  credentials: true
+}));
+
 // Middlewares básicos
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
