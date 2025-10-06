dce4ac9abf56ab4a4bd2a2c9c860ce8e35ec8cac
AndresPico <2005picogarcia@gmail.com>
2025-10-06 10:35:25 -0500

    Ajustes

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 // src/server.js
 const app = require("./app");
 
-const PORT = process.env.PORT || 4200;
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en Puerto: ${PORT}`);
-});
-
 app.use((req, res, next) => {
   console.log("➡️ Petición:", req.method, req.url);
   next();
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Servidor corriendo en puerto: ${PORT}`);
 });
